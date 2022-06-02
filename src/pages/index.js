f1a02949668e83de9583c11adc1c459a4b69e184
gareth-was-here <gareth@garethveale.dev>
2022-06-02 10:09:10 +0100

    routes + prettier support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -23,10 +24,12 @@ export default function Home() {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <h1 className="title">
+            Read{' '}
+            <Link href="/posts/first-post">
+              <a>this page!</a>
+            </Link>
+          </h1>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
@@ -54,8 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +207,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
